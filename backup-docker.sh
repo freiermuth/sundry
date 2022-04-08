555ae5f5e1b9500f27b9e3c46c6b3d80373b4946
Josh Freiermuth <freiermuthj@gmail.com>
2022-04-08 14:13:58 -0500

    adding backup-docker.sh

---
+++ backup-docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BACKUP_LABEL=us.freiermuth.backup
+BACKUP_CONTAINERS=$(docker ps -qaf "label=$BACKUP_LABEL")
+DATE=$(date '+%Y-%m-%d')
+BACKUP_DIR=/backup
+
+
+for c in $BACKUP_CONTAINERS
+do
+        echo "Stopping container $c..."
+        docker stop "$c"
+
+        IFS="
+        "
+        # Do backup
+        for each in `sudo docker inspect $c | jq '.[].Mounts[] | "\(.Name) \(.Source)"'`;
+        do
+                unset IFS
+                line=`echo $each | sed s/\"//g`
+                line=($line)
+                volume_name=${line[0]}
+                backup_file=$BACKUP_DIR/$volume_name-$DATE.tar.gz
+                mountpoint=${line[1]}
+                echo Backing up volume $volume_name at $mountpoint to $backup_file...
+                tar czvf $backup_file $mountpoint
+        done
+
+
+        docker start $c
+done
